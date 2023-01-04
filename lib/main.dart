44ae320c1628d7a21bdc996ae1315b76a98ab149
Rodrigo Samico Balter <fmce545@fmce.dc.local>
2023-01-04 16:36:59 -0300

    evoluindo com columns, container, stack

--- lib/main.dart
+++ lib/main.dart
@@ -15,16 +15,34 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter AA TESTE',
       theme: ThemeData(
-        primarySwatch: Colors.green,
+        primarySwatch: Colors.blue,
       ),
-      home:Stack(
-        alignment: AlignmentDirectional.center,
+      home: Container(
+        color: Colors.black,
+
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
+          Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
           Container(color: Colors.green, width: 100, height: 100,),
           Container(color: Colors.blue, width: 50, height: 50,),
-        ]
-      ),
+        ],
+        ),
+         Stack(
+            alignment: AlignmentDirectional.center,
+            children: [
+          Container(color: Colors.green, width: 100, height: 100,),
+          Container(color: Colors.blue, width: 50, height: 50,),
+        ],
+        )      
+        ],
+      )
+      )
     );
   }
+
 }
 //==================xxxxxxxxx===========================
