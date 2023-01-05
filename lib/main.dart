5149712840b9a42aa11ffebba561216a82575796
Rodrigo Samico Balter <fmce545@fmce.dc.local>
2023-01-05 09:52:30 -0300

    adicionado ListView para scroll, padding para espaçar info e overflow para conter no espaço

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,12 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
            title: Text('teste 1'),
       ),
-      body: Column(
+      body: ListView(
         children: [
-          Task('num1'),
-           Task('num2'),
-        ],
+          Task('num1dsdsdsdsddsdsd dsdsdsdsdsdsds'),
+          Task('num2'),
+          Task('num2'),
+         ],
       ),
       floatingActionButton: FloatingActionButton(onPressed: (){}),
       ),
@@ -41,31 +42,37 @@ class Task extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-            child: Stack(
-              children:[
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+              child: Stack(
+                children:[
  Container(
-            color: Colors.blue,
-            height: 140,
-          ),
-          Container(
-            color: Colors.white,
-            height: 100,
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-  children: [
-    Container(
-          color: Colors.black26,
-          width: 72,
-          height: 100,
-    ),
-    Text(nome),
-    ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
-],
+              color: Colors.blue,
+              height: 140,
             ),
-          )
-              ],
-            ),     
-          );
+            Container(
+              color: Colors.white,
+              height: 100,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+  children: [
+      Container(
+       width: 200,
+       child: Text(
+        nome, 
+        style: TextStyle(
+          fontSize: 24,
+          overflow: TextOverflow.ellipsis,
+        ),
+      )),
+      ElevatedButton(onPressed: (){}, child: Icon(Icons.arrow_drop_up))
+],
+              ),
+            )
+                ],
+              ),     
+            ),
+    );
   }
 }
